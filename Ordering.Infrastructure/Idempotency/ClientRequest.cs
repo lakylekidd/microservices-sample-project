5944f22941b2e0097686ba3cfa807511094d3311
Billy Vlachos <williamcaminiti@live.com>
2019-10-09 22:18:06 +0200

    Implement entity type configurations and build context

---
+++ Ordering.Infrastructure/Idempotency/ClientRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace App.Services.Ordering.Infrastructure.Idempotency
+{
+    public class ClientRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Ordering.Infrastructure/OrderingContext.cs
@@ -0,0 +1,194 @@
+﻿using MediatR;
+using System;
+using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Storage;
+
+using Ordering.Domain.AggregatesModel.BuyerAggregate;
+using Ordering.Domain.AggregatesModel.OrderAggregate;
+using Ordering.Domain.SeedWork;
+using Ordering.Infrastructure.EntityConfigurations;
+
+namespace Ordering.Infrastructure
+{
+    public class OrderingContext : DbContext, IUnitOfWork
+    {
+        /// <summary>
+        /// The default schema of the database
+        /// </summary>
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        private readonly IMediator _mediator;
+        private IDbContextTransaction _currentTransaction;
+
+        #region ENTITY DBSETS
+        // All the available entities in the context
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<PaymentMethod> Payments { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+        public DbSet<CardType> CardTypes { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
+
+        #endregion   
+
+        private OrderingContext(DbContextOptions<OrderingContext> options) 
+            : base(options) { }
+
+        public OrderingContext(DbContextOptions<OrderingContext> options, IMediator mediator) : base(options)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            System.Diagnostics.Debug.WriteLine("OrderingContext::ctor ->" + this.GetHashCode());
+        }
+
+        /// <summary>
+        /// Gets the current transaction
+        /// </summary>
+        /// <returns></returns>
+        public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+
+        /// <summary>
+        /// Checks if there are currently any active transactions
+        /// </summary>
+        public bool HasActiveTransaction => _currentTransaction != null;        
+
+        /// <summary>
+        /// Method that runs when the context is created. It will apply all the entity type configurations
+        /// defined under the entity configurations folder.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ClientRequestEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new PaymentMethodEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderItemEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new CardTypeEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderStatusEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new BuyerEntityTypeConfiguration());
+        }
+
+        /// <summary>
+        /// Public async function that saves all the entities asynchronously
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Dispatch Domain Events collection. 
+            // Choices:
+            // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+            // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+            await _mediator.DispatchDomainEventsAsync(this);
+
+            // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+            // performed through the DbContext will be committed
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Public async function that begins a transaction asynchronously
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            if (_currentTransaction != null) return null;
+
+            _currentTransaction = await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
+
+            return _currentTransaction;
+        }
+
+        /// <summary>
+        /// Public async function that commits a transaction asynchronously
+        /// </summary>
+        /// <param name="transaction"></param>
+        /// <returns></returns>
+        public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+        {
+            if (transaction == null) throw new ArgumentNullException(nameof(transaction));
+            if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+
+            try
+            {
+                await SaveChangesAsync();
+                transaction.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Public async function that rolls back the current transaction
+        /// </summary>
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+    }
+
+    public class OrderingContextDesignFactory : IDesignTimeDbContextFactory<OrderingContext>
+    {
+        public OrderingContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderingContext>()
+                .UseSqlServer("Server=.;Initial Catalog=Microsoft.eShopOnContainers.Services.OrderingDb;Integrated Security=true");
+
+            return new OrderingContext(optionsBuilder.Options, new NoMediator());
+        }
+
+        class NoMediator : IMediator
+        {
+            public Task Publish<TNotification>(TNotification notification, CancellationToken cancellationToken = default(CancellationToken)) 
+                where TNotification : INotification
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task Publish(object notification, CancellationToken cancellationToken = default)
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                return Task.FromResult<TResponse>(default(TResponse));
+            }
+
+            public Task Send(IRequest request, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
