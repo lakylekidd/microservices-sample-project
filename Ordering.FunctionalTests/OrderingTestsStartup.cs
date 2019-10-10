602eec1856aa72a24c54e3e0a1903dad63418c9d
Billy Vlachos <williamcaminiti@live.com>
2019-10-10 19:36:07 +0200

    Create startup container to be used for functional testing of entire api

---
+++ Ordering.FunctionalTests/OrderingTestsStartup.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.Configuration;
+
+using Ordering.API;
+
+namespace Ordering.FunctionalTests
+{
+    public class OrderingTestsStartup : Startup
+    {
+        public OrderingTestsStartup(IConfiguration env) : base(env)
+        { }
+    }
+}
