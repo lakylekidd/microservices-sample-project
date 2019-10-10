4981377f9f910ee87f153673ac79dda9441d3d5c
Billy Vlachos <williamcaminiti@live.com>
2019-10-10 19:10:27 +0200

    Create and implement the mediator module

--- Ordering.API/Infrastructure/AutofacModules/MediatorModule.cs
+++ Ordering.API/Infrastructure/AutofacModules/MediatorModule.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using MediatR;
+using Ordering.API.Application.Commands;
 using System.Reflection;
 
 namespace Ordering.API.Infrastructure.AutofacModules
@@ -8,8 +9,19 @@ namespace Ordering.API.Infrastructure.AutofacModules
     {
         protected override void Load(ContainerBuilder builder)
         {
+            // Retrieve the assembly information of the current project
+            var assembly = typeof(Startup).GetTypeInfo().Assembly;
+
             // Register MediatR with Autofac
             builder.RegisterAssemblyModules(typeof(IMediator).GetTypeInfo().Assembly);
+
+            // Register all the classes that implement the following
+            // interfaces and that reside in the current assembly.
+            // ATTN: If we decide to change the location of these classes (commands and domain events)
+            // Then we should consider scanning in the new assembly they will reside.
+            builder.RegisterAssemblyTypes(assembly)
+                .AsClosedTypesOf(typeof(IRequestHandler<,>))        // Register command classes that implement IRequestHandler
+                .AsClosedTypesOf(typeof(INotificationHandler<>));   // Register domain event classes that implement INotificationHandler
         }
     }
 }
